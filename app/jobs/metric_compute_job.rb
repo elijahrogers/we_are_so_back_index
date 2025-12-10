0fbb8aae5bc28960ed342df8dfdeda3d35dba8ac
Elijah Rogers <elijah@elijahrogers.dev>
2025-12-09 21:46:09 -0800

    Use correct timezone for snapshot dates

--- app/jobs/metric_compute_job.rb
+++ app/jobs/metric_compute_job.rb
@@ -1,7 +1,8 @@
 class MetricComputeJob < ApplicationJob
   queue_as :default
 
-  def perform(as_of: Date.current)
+  # Store dates in the US timezone because trading days are specific to the US
+  def perform(as_of: ActiveSupport::TimeZone["Pacific Time (US & Canada)"].today)
     payload = Metrics::SnapshotBuilder.new(as_of: as_of).call
     attributes = payload.compact
     return if attributes.empty?
