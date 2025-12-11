db214d93cb2b4bbc6a936e96ee1c39f78f68b5e8
Elijah Rogers <elijah@elijahrogers.dev>
2025-12-11 08:21:07 -0800

    Set latest metric correctly

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,6 @@
 class IndexController < ApplicationController
   def show
-    @metric = MetricSnapshot.last
     @metrics = MetricSnapshot.order(date: :desc).limit(30).reverse
+    @metric = @metrics.last
   end
 end
